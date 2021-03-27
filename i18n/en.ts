5742a68261b7a06e871c648b69d399fb4eedd338
VincentJonathan <vinjongm@gmail.com>
2021-03-27 11:18:44 +0700

    initial commit

---
+++ i18n/en.ts
@@ -0,0 +1,2 @@
+export const locale = "en";
+export const messages = {};
---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+import i18n from "./i18n";
+
+export default {
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: "cheese.formice.com",
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ hid: "description", name: "description", content: "" },
+		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+	},
+
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: ["ant-design-vue/dist/antd.css", "@/assets/global.css"],
+
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: ["@/plugins/antd-ui"],
+
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
+
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/typescript
+		"@nuxt/typescript-build",
+	],
+
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		// https://go.nuxtjs.dev/axios
+		"@nuxtjs/axios",
+		// https://go.nuxtjs.dev/pwa
+		"@nuxtjs/pwa",
+		"nuxt-i18n",
+	],
+
+	i18n: {
+		locales: i18n.map((i) => i.locale),
+		defaultLocale: "en",
+		vueI18n: {
+			fallbackLanguage: "en",
+			messages: i18n.reduce((lang, i) => (lang[i.locale] = i.messages), {}),
+		},
+	},
+
+	// Axios module configuration: https://go.nuxtjs.dev/config-axios
+	axios: {},
+
+	// PWA module configuration: https://go.nuxtjs.dev/pwa
+	pwa: {
+		manifest: {
+			lang: "en",
+		},
+	},
+
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
+};
