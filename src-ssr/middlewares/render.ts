68d9b48ff6c4008a53464042a974b2572f00dbdc
VincentJonathan <vinjongm@gmail.com>
2021-05-03 10:36:40 +0700

    use async await

--- src-ssr/middlewares/render.ts
+++ src-ssr/middlewares/render.ts
@@ -5,27 +5,23 @@ import { ssrMiddleware } from "quasar/wrappers";
 // render the page with Vue
 
 export default ssrMiddleware(({ app, resolve, render, serve }) => {
-  app.get(resolve.urlPath("*"), (req, res) => {
+  app.get(resolve.urlPath("*"), async (req, res) => {
     res.setHeader("Content-Type", "text/html");
 
-    render({ req, res })
-      .then((html) => {
-        res.send(html);
-      })
-      .catch((err) => {
-        if (err.url) {
-          if (err.code) {
-            res.redirect(err.code, err.url);
-          } else {
-            res.redirect(err.url);
-          }
-        } else if (err.code === 404) {
-          res.status(404).send("404 | Page Not Found");
-        } else if (process.env.DEV) {
-          serve.error({ err, req, res });
-        } else {
-          res.status(500).send("500 | Internal Server Error");
-        }
-      });
+    try {
+      const html = await render({ req, res });
+      res.send(html);
+    } catch (err) {
+      if (err.url) {
+        if (err.code) res.redirect(err.code, err.url);
+        else res.redirect(err.url);
+      } else if (err.code === 404) {
+        res.status(404).send("404 | Page Not Found");
+      } else if (process.env.DEV) {
+        serve.error({ err, req, res });
+      } else {
+        res.status(500).send("500 | Internal Server Error");
+      }
+    }
   });
 });
