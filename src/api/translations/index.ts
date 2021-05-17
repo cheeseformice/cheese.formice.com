30e436746e74730bbc83b0ca4e017e1b905e0333
Iván <tocutoeltuco@gmail.com>
2021-05-17 00:12:47 +0000

    Show title under player look

---
+++ src/api/translations/index.ts
@@ -0,0 +1,2 @@
+export { default as TranslationsService } from "./service";
+export * from "./interfaces";
--- src/store/modules/Player.ts
+++ src/store/modules/Player.ts
@@ -1,14 +1,20 @@
 import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
-import { Player, PlayersService, PlayerChangelogTypes as T, PlayerChangelogs } from "src/api";
+import { Player, PlayersService, TranslationsService, PlayerChangelogTypes as T, PlayerChangelogs } from "src/api";
 
 @Module({ namespaced: true, name: "player", stateFactory: true })
 export default class PlayerModule extends VuexModule {
   player: Player | null = null;
+  title: string | null = null;
   changelogsRaw: PlayerChangelogs<T.Normal | T.Survivor | T.Racing | T.Defilante> | null = null;
 
   @Mutation
   setPlayer(player: Player | null) {
     this.player = player;
+  }
+
+  @Mutation
+  setTitle(title: string | null) {
+    this.title = title;
   }
 
   @Mutation
@@ -19,13 +25,30 @@ export default class PlayerModule extends VuexModule {
   @Mutation
   resetState() {
     this.player = null;
+    this.title = null;
     this.changelogsRaw = null;
   }
 
   @Action
   async getPlayer(name: string) {
     const response = await PlayersService.getById(name);
-    this.setPlayer(response.status === 200 ? response.data : null);
+    if (response.status === 200) {
+      this.setPlayer(response.data);
+      await this.getTitle(response.data.title);
+    } else {
+      this.setPlayer(null);
+      this.setTitle(null);
+    }
+    return response;
+  }
+
+  @Action
+  async getTitle(title: number) {
+    const response = await TranslationsService.fetchFields({
+      fields: [`T_${title}`]
+    });
+    // weirdly enough, axios returns the keys in lowercase...
+    this.setTitle(response.status === 200 ? response.data[`t_${title}`] : null);
     return response;
   }
 
