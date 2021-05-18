2b130906f4bbebdf52547547917a2af4cd2d309f
Iván <tocutoeltuco@gmail.com>
2021-05-18 18:29:25 +0000

    Add tribe changelogs to the API

--- src/api/tribes/interfaces.ts
+++ src/api/tribes/interfaces.ts
@@ -8,6 +8,36 @@ export interface Tribe extends BaseTribe {
   stats: Stats;
 }
 
+export enum TribeChangelogTypes {
+  Members = 1,
+  Active = 2,
+  Shaman = 4,
+  Normal = 8,
+  Survivor = 16,
+  Racing = 32,
+  Defilante = 64,
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface TribeChangelogs<T extends number = any> {
+  id: number;
+  name: string;
+  dates: string[];
+  members: T extends TribeChangelogTypes.Members ? Log<number> : undefined;
+  active: T extends TribeChangelogTypes.Active ? Log<number> : undefined;
+  normal: T extends TribeChangelogTypes.Normal ? Log<Normal> : undefined;
+  shaman: T extends TribeChangelogTypes.Shaman ? Log<Shaman> : undefined;
+  survivor: T extends TribeChangelogTypes.Survivor ? Log<Survivor> : undefined;
+  racing: T extends TribeChangelogTypes.Racing ? Log<Racing> : undefined;
+  defilante: T extends TribeChangelogTypes.Defilante ? Log<Defilante> : undefined;
+}
+
+type DateIndex = number;
+
+type Log<T> = {
+  [key in keyof T]: [DateIndex, number | string][];
+};
+
 export interface TribeLeaderboard {
   id: number;
   name: string;
