0ad67eddf81ad3e97ad5986fddb3ef1bedcfe3ea
VincentJonathan <vinjongm@gmail.com>
2021-05-24 08:43:36 +0700

    add language selector

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -15,22 +15,50 @@
       :key="section.title"
       :title="section.title"
       :items="section.items"
-    />
+    >
+      <template v-slot:Preference>
+        <q-list dense class="q-px-sm">
+          <q-item style="padding-left: 0; padding-right: 0">
+            <q-select
+              dark
+              v-model="language"
+              :options="languageOptions"
+              dense
+              borderless
+              emit-value
+              map-options
+              options-dense
+            />
+          </q-item>
+        </q-list>
+      </template>
+    </footer-section>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-property-decorator";
-import { FooterItem } from "./FooterSection.vue";
-import FooterSection from "./FooterSection.vue";
+import { Options, Vue, Watch } from "vue-property-decorator";
+import i18n from "src/i18n";
+import FooterSection, { FooterItem } from "./FooterSection.vue";
 
 interface FooterSectionInfo {
   title: string;
-  items: FooterItem[];
+  items?: FooterItem[];
 }
 
 @Options({ components: { FooterSection } })
 export default class AppFooter extends Vue {
+  language = "en";
+
+  get languageOptions() {
+    const languages = Object.keys(i18n) as Array<keyof typeof i18n>;
+
+    return languages.map((l) => ({
+      value: l,
+      label: i18n[l].languageName,
+    }));
+  }
+
   get sections(): FooterSectionInfo[] {
     return [
       {
@@ -68,19 +96,14 @@ export default class AppFooter extends Vue {
         ],
       },
       {
-        title: "Contribute",
-        items: [
-          {
-            label: "Translation",
-            to: { name: "translation" },
-          },
-          {
-            label: "Source code",
-            to: { name: "github" },
-          },
-        ],
+        title: "Preference",
       },
     ];
+  }
+
+  @Watch("language")
+  onLanguageChange() {
+    this.$i18n.locale = this.language;
   }
 }
 </script>
--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="col-6 col-md-2 q-pb-md">
     <div class="text-grey q-px-sm text-uppercase text-bold">{{ title }}</div>
-    <q-list dense class="q-px-sm">
-      <q-item v-for="item in items" :key="item.label" style="padding-left: 0; padding-right: 0">
-        <q-item-section>
-          <template v-if="item.link">
-            <a :href="item.link" target="_blank" class="text-white">{{ item.label }}</a>
-          </template>
-          <template v-else-if="item.to">
-            <router-link :to="item.to" class="text-white">{{ item.label }}</router-link>
-          </template>
-          <template v-else>
-            {{ item.label }}
-          </template>
-        </q-item-section>
-      </q-item>
-    </q-list>
+    <slot :name="title">
+      <q-list dense class="q-px-sm">
+        <q-item v-for="item in items" :key="item.label" style="padding-left: 0; padding-right: 0">
+          <q-item-section>
+            <template v-if="item.link">
+              <a :href="item.link" target="_blank" class="text-white">{{ item.label }}</a>
+            </template>
+            <template v-else-if="item.to">
+              <router-link :to="item.to" class="text-white">{{ item.label }}</router-link>
+            </template>
+            <template v-else>
+              {{ item.label }}
+            </template>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </slot>
   </div>
 </template>
 
@@ -33,6 +35,6 @@ export interface FooterItem {
 
 export default class FooterSection extends Vue {
   @Prop({}) title!: string;
-  @Prop({}) items!: FooterItem[];
+  @Prop({ default: () => [] }) items!: FooterItem[];
 }
 </script>
