621e62b22358f477da76ca9348681d797cb05c5a
Iván <tocutoeltuco@gmail.com>
2021-05-26 17:31:01 +0000

    Change beta domain

--- src/common/vars.ts
+++ src/common/vars.ts
@@ -3,7 +3,7 @@ let tmp: string;
 switch (window.location.hostname) {
   case "cfmtest.tk":
   case "cheese.formice.com":
-  case "beta.cheese.formice.com":
+  case "beta.formice.com":
     tmp = window.location.hostname;
 
   default:
