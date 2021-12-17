ba25ce8c6a01fd98d414f59ba0d643b5a18b09f5
Iván Gabriel <tocutoeltuco@gmail.com>
2021-12-17 13:45:13 -0300

    Allow http in test domains

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,9 +3,12 @@ import axios from "axios";
 import applyCaseMiddleware from "axios-case-converter";
 import { DOMAIN } from "src/common/vars";
 
+const http = DOMAIN !== "cheese.formice.com" ? window.location.protocol : "https:";
+const ws = http === "http:" ? "ws:" : "wss:";
+
 const URL = `${DOMAIN}/api`;
-export const API_BASE_URL = `https://${URL}`;
-export const WS_BASE_URL = `wss://${URL}`;
+export const API_BASE_URL = `${http}//${URL}`;
+export const WS_BASE_URL = `${ws}//${URL}`;
 
 const api = applyCaseMiddleware(
   axios.create({
